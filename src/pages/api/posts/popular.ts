5ca9bac0f3fbad2dfd064977e8dff67f4b74ab97
Robin Andeer <robin.andeer@gmail.com>
2021-04-23 12:48:58 +0200

    Add google verification
    Add popular posts endpoint.

---
+++ src/pages/api/posts/popular.ts
@@ -0,0 +1,11 @@
+import { getPopularBlogPosts } from 'database'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    const blogPosts = await getPopularBlogPosts()
+    res.status(200).json({ blogPosts })
+  } else {
+    res.status(400)
+  }
+}
