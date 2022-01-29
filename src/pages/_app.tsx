61e6dbea501f4204b195fb2f171727adfb60a155
Robin Andeer <robin@hedvig.com>
2022-01-29 20:18:14 +0100

    Small fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,10 +34,7 @@ const MyApp: FC<AppProps> = ({Component, pageProps}) => {
 	return (
 		<>
 			<Head>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, minimal-ui"
-				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1"/>
 			</Head>
 			<Component {...pageProps}/>
 		</>
