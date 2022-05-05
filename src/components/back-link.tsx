da94a363325717eb3ad4d4e647379973406e6a37
Robin Andeer <robin.andeer@gmail.com>
2022-05-05 22:38:22 +0200

    Fix eslint issue

--- src/components/back-link.tsx
+++ src/components/back-link.tsx
@@ -5,7 +5,7 @@ interface Props {
 	children: React.ReactNode;
 }
 
-const BackLink = React.forwardRef<HTMLAnchorElement, Props>(({children, ...props}, ref) => (
+const BackLink: React.ForwardRefRenderFunction<HTMLAnchorElement, Props> = (({children, ...props}, ref) => (
 	<a className="text-gray-700 dark:text-gray-300 text-base hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-50 active:bg-gray-200 dark:active:bg-gray-600 rounded inline-block px-2 py-1 -ml-2 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50" ref={ref} {...props}>
 		<div className="flex items-center gap-1">
 			<RiArrowLeftCircleFill className="w-5 h-5"/>
@@ -14,4 +14,4 @@ const BackLink = React.forwardRef<HTMLAnchorElement, Props>(({children, ...props
 	</a>
 ));
 
-export default BackLink;
+export default React.forwardRef(BackLink);
